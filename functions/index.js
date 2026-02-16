632e529be9141856d2da95adfd47c89fef396a65
Derflabesog <wildernesafrica@gmail.com>
2026-02-16 10:09:49 +0000

    Deploy Firebase hosting + ping function

---
+++ functions/index.js
@@ -0,0 +1,8 @@
+const functions = require("firebase-functions");
+const admin = require("firebase-admin");
+
+admin.initializeApp();
+
+exports.ping = functions.https.onRequest((req, res) => {
+  res.json({ ok: true, message: "Universal Edu Platform API is live ✅" });
+});
